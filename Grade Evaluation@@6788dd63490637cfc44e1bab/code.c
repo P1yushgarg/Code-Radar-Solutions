c56ab81c54543e5dd747c7a738a7b9903b39bed2
Piyush garg <piyushgarg192006@gmail.com>
2025-01-27 00:45:38 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main(){
+    char c;
+    scanf("%c",&c);
+    if(c=='A'){
+        printf("Excellent");
+    }
+    else if(ch=='B'){
+        printf("Good");
+    }
+    else if(ch=='C'){
+        printf("Average");
+    }
+    else if(ch=='D'){
+        printf("Below Average");
+    }
+    else if(ch=='F'){
+        printf("Fail");
+    }
+}
