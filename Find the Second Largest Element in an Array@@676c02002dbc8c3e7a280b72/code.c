9cfe129e1ad87e7ea970ffad74a97fdecdf7e729
Piyush garg <piyushgarg192006@gmail.com>
2025-02-07 19:16:01 +0530

    Solution to Find the Second Largest Element in an Array

---
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+    int n,i,max;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    max=a[0]
+    for(i=0;i<n;i++){
+        if(a[i]>max){
+            printf("%d",a[i-1]);
+        }
+    }
+}
