83f753cc29017d02529da4bcb7030965fb4533df
Piyush garg <piyushgarg192006@gmail.com>
2025-02-13 13:05:55 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,31 +1,58 @@
-#include <stdio.h>
-#include <limits.h>  // Include this for INT_MIN
-
-void main() {
-    int n, i, max, second_max;
-    scanf("%d", &n);
+// #include <stdio.h>
+// #include <limits.h>
+// void main() {
+//     int n, i, max, second_max;
+//     scanf("%d", &n);
     
+//     int a[n];
+//     for(i = 0; i < n; i++) {
+//         scanf("%d", &a[i]);
+//     }
+
+//     max = second_max = INT_MIN;
+
+//     for(i = 0; i < n; i++) {
+//         if(a[i] > max) {
+//             second_max = max;
+//             max = a[i];
+//         }
+//         else if(a[i] > second_max && a[i] != max) {
+//             second_max = a[i];
+//         }
+//     }
+
+//     if(second_max != INT_MIN) {
+//         printf("%d", second_max);
+//     }
+//     else {
+//         printf("-1");
+//     }
+// }
+
+
+#include<stdio.h>
+#include<limit.h>
+void main(){
+    int i,n,max,second_max;
+    scanf("%d",n);
     int a[n];
-    for(i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    for(i=0;i<n;i++){
+        scanf("%d",a[i]);
     }
-
-    max = second_max = INT_MIN;  // Initialize with smallest possible integer
-
-    for(i = 0; i < n; i++) {
-        if(a[i] > max) {
-            second_max = max;
-            max = a[i];
+    max=second_max=INT_MIN;
+    for(i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
         }
-        else if(a[i] > second_max && a[i] != max) {
-            second_max = a[i];
+        else if(a[i]>second_max&&a[i]!=max){
+            second_max=a[i];
         }
-    }
 
-    if(second_max != INT_MIN) {
-        printf("%d", second_max);
     }
-    else {
+    if(second_max!=INT_MIN){
+        printf("%d",second_max);
+    }
+    else{
         printf("-1");
     }
 }
