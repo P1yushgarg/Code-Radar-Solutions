496f4aa292ea11cec476fa891248d984d02f7fc0
Piyush garg <piyushgarg192006@gmail.com>
2025-03-09 11:17:53 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,26 +1,38 @@
-#include<stdio.h>
-void selectionSort(int arr[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1])
-            int c=arr[j];
-            arr[j+1]=arr[j];
-            arr[j+1]=c;
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i; // Assume the first element is the minimum
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j; // Update minimum index
+            }
         }
+        // Swap arr[i] and arr[minIndex]
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
-void printArray(int arr[],int n){
-    for(int i=0;i<n-1;i++){
-        return arr[i];
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) { // Fixed loop condition
+        printf("%d ", arr[i]);
     }
+    printf("\n");
 }
-void main(){
+
+int main() { // Standard 'main' signature
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    selectionSort(arr,n);
-    printArray(arr,n);
+    
+    selectionSort(arr, n);
+    printArray(arr, n); // Properly prints the sorted array
+
+    return 0; // Indicating successful execution
 }
