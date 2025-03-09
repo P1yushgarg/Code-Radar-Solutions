4abcb2ccbf40d97d4aa1e42019acfb61567bf76e
Piyush garg <piyushgarg192006@gmail.com>
2025-03-09 11:18:23 +0530

    Solution to Bubble Sort Implementation

---
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int isPrime(int num){
+    int c=0,i;
+    for(i=1;i<num;i++){
+        if(num%i==0){
+            c++;
+        }
+    }
+    if(c==1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int num;
+        scanf("%d",&num);
+        printf("%d\n",isPrime(num));
+    }
+    return 0;
+}
