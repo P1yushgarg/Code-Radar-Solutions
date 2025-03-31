437350e7c076430d7f133d7a773f18a74ac5616f
Piyush garg <piyushgarg192006@gmail.com>
2025-04-01 00:54:47 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,36 +1,22 @@
-#include <stdio.h>
-
-void Sum(int arr[], int n, int T) {
-    int printedPairs[n][n]; // To track printed pairs (initialize with 0)
-    
-    // Initialize tracking array
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printedPairs[i][j] = 0;
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) { 
-            if (arr[i] + arr[j] == T && !printedPairs[i][j]) {
-                printf("%d %d\n", arr[i], arr[j]); 
-                
-                // Mark the pair as printed
-                printedPairs[i][j] = 1;
-                printedPairs[j][i] = 1; // Avoid reversing order
+#include<stdio.h>
+void solve(int a[],int n,int t){
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(a[i]+a[j]==t){
+                printf("%d %d",a[i],a[j]);
             }
         }
     }
 }
-
-int main() {
-    int n, T;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
     }
-    scanf("%d", &T);
-    Sum(arr, n, T);
+    int t;
+    scanf("%d",&t);
+    solve(a,n,t);
     return 0;
 }
