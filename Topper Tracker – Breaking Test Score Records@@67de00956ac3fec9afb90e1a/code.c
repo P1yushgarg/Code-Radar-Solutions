88a6fb9d76b1edadf69e025060c1f922572d0598
Piyush garg <piyushgarg192006@gmail.com>
2025-04-08 18:35:26 +0530

    Solution to Topper Tracker – Breaking Test Score Records

--- Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
+++ Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
@@ -2,7 +2,7 @@ int trackScores(int scores[],int n,int result[]){
     int i,high=0,low=0;
     for(i=1;i<n;i++){
         int r=scores[0];
-        if(score[i]>r){
+        if(scores[i]>r){
             result[high++];
         }
         else if(scores[i]<r){
