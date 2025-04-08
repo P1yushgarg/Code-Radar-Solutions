19d1e12ca56b32fdc469258d7cd7c28a21f54131
Piyush garg <piyushgarg192006@gmail.com>
2025-04-08 18:05:04 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,10 +1,12 @@
 int trackPlayerRanks(int ranked [],int n,int player[],int m,int result[]){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(player[0]<ranked[i]){
-                printf("%d",i+1);
-            }
+    int s=0,i;
+    for(i=0;i<m;i++){
+        s=s+player[i];
+    } 
+    for(i=0;i<n;i++){
+        if(ranked[i]>s){
+            break;
         }
     }
-    
+    return i;
 }
