7f726fe0dd446ce479ebe430c9fea46f7ba4e311
Piyush garg <piyushgarg192006@gmail.com>
2025-04-09 10:54:39 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -1,7 +1,7 @@
 int compressString(char str[],int compressed){
     compressed=0;
     for(int i=0;str[i]!='\0';i++){
-        for(int j=0;str[i]!='\0';j++){
+        for(int j=0;str[j]!='\0';j++){
             if(str[i]==str[j]){
                 compressed++;
             }
